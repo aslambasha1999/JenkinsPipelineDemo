dfd960279328e7b39baa72173e09810e9c7cceaa
Aslam Basha <aslam.basha1999@gmail.com>
2022-01-04 14:54:01 +0530

    added scanner

--- AdditionofTwoNumbers.java
+++ AdditionofTwoNumbers.java
@@ -3,8 +3,10 @@ public class AdditionofTwoNumbers {
 	
 	public static void main(String args[])
 	{
-		int a=10;
-		int b=20;
+		Scanner sc=new Scanner(System.in);
+		
+		int a=sc.nextInt();
+		int b=sc.nextInt();
 		int c=a+b;
 		System.out.println("sum of a and b is "+ c ); 
 		
